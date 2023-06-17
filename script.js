cd19d7201175cc08c40d4e2977708f5d5fea3944
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-06-17 12:24:49 +0300

    pluses-and-hashes assignment - Done

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Good Luck 💪🏾
+
+
+function plusHashCount(str) {
+    let hashCount = 0;
+    let plusCount = 0;
+  
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === '#') {
+        hashCount++;
+      } else if (str[i] === '+') {
+        plusCount++;
+      }
+    }
+  
+    return { hashes: hashCount, pluses: plusCount };
+  }
+  
+  const result = plusHashCount("##+#+#+#+++");
+  console.log(result);
